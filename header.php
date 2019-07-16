5dfe89b23c7825420dd5ec6aee51a83b2a037c54
Katie Woods <kfw8@hood.edu>
2019-07-16 18:26:12 -0400

    Tentatively finalized homepage layout

--- header.php
+++ header.php
@@ -6,9 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- Bootstrap -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<!-- Font Awesome icons -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/solid.css" integrity="sha384-+0VIRx+yz1WBcCTXBkVQYIBVNEFH1eP6Zknm16roZCyeNg2maWEpk/l/KsyFKs7G" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/fontawesome.css" integrity="sha384-jLuaxTTBR42U2qJ/pm4JRouHkEDHkVqH0T1nyQXn1mZ7Snycpf6Rl25VBNthU4z0" crossorigin="anonymous">
--- home.php
+++ home.php
@@ -32,47 +32,20 @@ function build_card($table) {
     $tbl_link = "other.php";
     $btn_class = "o";
   }
-  echo '<div class="col-sm-3"><div class="card text-center">';
+  echo '<div class="col-md-3"><div class="card text-center">';
   echo '<div class="card-header"><a href="'.$tbl_link.'" class="btn btn-lg btn-'.$btn_class.'">'.$header.'</a></div>';
   echo '<a href="'.$rand_link.'"><img src="'.$rand_spp['img_url'].'" class="card-img-top" alt="'.$table.'"></a>';
   echo '<div class="card-body text-muted sec-'.$btn_class.'">'.$rand_spp['common_name'].'<br/><em>'.$rand_spp['latin_name'].'</em></div>';
     echo '</div></div>';
 }
 ?>
-<div class="jumbotron">
-  <div class="col">
-    <h1 class="display-4">Wildlife logs</h1>
-    <p class="lead">Here's some information about what species you've seen recently.</p>
-    <p><a href="new_log.php" class="btn btn-d btn-lg">New log</a></p>
-  </div>
-</div>
-  <div class="row">
+<div class="container">
+  <div class="jumbotron">
     <div class="col">
-      <div class="text-center">&nbsp;</div>
-
-      <?php
-        $stmt = $conn->prepare("SELECT latin_name, img_url, date FROM Log JOIN Creature USING (latin_name) WHERE TIMESTAMPDIFF(DAY, date, CURDATE()) <= 7 ORDER BY date DESC, latin_name ASC");
-        $stmt->execute();
-        $thumbs = $stmt->fetchAll();
-        for ($i = 0; $i < count($thumbs); $i++) {
-          $url = str_replace('l.', 't.', $thumbs[$i]['img_url']);
-          $now = date_create(date('Y-m-d'));
-          $then = date_create($thumbs[$i]['date']);
-          $age = (int)date_diff($now, $then)->format('%d');
-          #$opacity = abs(20-$age)/10;
-          echo '<a href="view.php?spp='.$thumbs[$i]['latin_name'].'"><img src="'.$url.'" height=70></a>';
-        }
-      ?>
+      <h1 class="display-4">Species profiles</h1>
+      <p class="lead">View and search categories of plants, insects, and animals by clicking one of the buttons below.</p>
     </div>
   </div>
-</div>
-<div class="jumbotron">
-  <div class="col">
-    <h1 class="display-4">Species profiles</h1>
-    <p class="lead">View and search categories of plants, insects, and animals by clicking one of the buttons below.</p>
-  </div>
-</div>
-<div class="container">
   <div class="row">
       <?php
       build_card("Plant");
@@ -80,6 +53,86 @@ function build_card($table) {
       build_card("Bee_full");
       build_card("Other");
       ?>
+  </div>
+<div>&nbsp;</div>
+  <div class="jumbotron">
+    <div class="col">
+      <h1 class="display-4">Wildlife logs</h1>
+      <p class="lead">A summary of recent and lifetime sightings.</p>
+      <p><a href="new_log.php" class="btn btn-d btn-lg" style="color: white">New log</a></p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="card">
+        <div class="card-header d-flex justify-content-between"><strong>Today:</strong> <span>10 spp</span></div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item d-flex justify-content-between"><strong>Past 7 days:</strong> <span>10 spp, 30 logs</span></li>
+          <li class="list-group-item d-flex justify-content-between"><strong>Past 30 days:</strong> <span>35 spp, 67 logs</span></li>
+          <li class="list-group-item d-flex justify-content-between"><strong>This year:</strong> <span>109 spp, 342 logs</span></li>
+        </ul>
+      </div>
+      <div>&nbsp;</div>
+      <div class="card">
+        <ul class="list-group list-group-flush">
+          <li class="sec-l list-group-item d-flex justify-content-between"><strong>Butterflies:</strong> <span>45 / 75 spp</span></li>
+          <li class="list-group-item d-flex justify-content-between">Hesperiidae</li>
+          <li class="list-group-item d-flex justify-content-between">Lycaenidae</li>
+          <li class="list-group-item d-flex justify-content-between">Nymphalidae</li>
+          <li class="list-group-item d-flex justify-content-between">Papilionidae</li>
+          <li class="list-group-item d-flex justify-content-between">Pieridae</li>
+          <li class="sec-l list-group-item d-flex justify-content-between"><strong>Moths:</strong> <span>10 / 100 spp</span></li>
+        </ul>
+      </div>
+      <div>&nbsp;</div>
+      <div class="card">
+        <ul class="list-group list-group-flush">
+          <li class="sec-b list-group-item d-flex justify-content-between"><strong>Bees:</strong> <span>34 / 82 spp</span></li>
+          <li class="list-group-item d-flex justify-content-between">Halictidae</li>
+          <li class="list-group-item d-flex justify-content-between">Andrenidae</li>
+          <li class="list-group-item d-flex justify-content-between">Megachilidae</li>
+          <li class="list-group-item d-flex justify-content-between">Apidae</li>
+          <li class="list-group-item d-flex justify-content-between">Colletidae</li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-sm-8">
+      <div class="card">
+        <div class="card-header">Today's species</div>
+        <div class="card-body text-center">
+        <?php
+          $stmt = $conn->prepare("SELECT latin_name, common_name, img_url FROM Log JOIN Creature_full USING (latin_name) WHERE TIMESTAMPDIFF(DAY, date, CURDATE()) = 0 ORDER BY type, subtype, family_name, latin_name");
+          $stmt->execute();
+          $thumbs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          $curline = -1;
+          for ($i = 0; $i < count($thumbs); $i++) {
+            $url = str_replace('l.', 't.', $thumbs[$i]['img_url']);
+            echo '<a href="view.php?spp='.$thumbs[$i]['latin_name'].'">';
+            if ($url != '') echo '<img src="'.$url.'" width=80></a>';
+            else echo '<div style="width:80px; height:80px; display:inline-block; background-color:#e9ecef; vertical-align: middle">&nbsp;</div></a>';
+          }
+        ?>
+        </div>
+      </div>
+      <div>&nbsp;</div>
+      <div class="card">
+        <div class="card-header">Recent species</div>
+        <div class="card-body text-center">
+        <?php
+          $stmt = $conn->prepare("SELECT DISTINCT latin_name, common_name, img_url FROM (SELECT latin_name, common_name, img_url, date FROM Log JOIN Creature_full USING (latin_name) WHERE TIMESTAMPDIFF(DAY, date, CURDATE()) <= 30 ORDER BY date DESC, type, subtype, family_name, latin_name) Past_logs LIMIT 40");
+          $stmt->execute();
+          $thumbs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          $curline = -1;
+          for ($i = 0; $i < count($thumbs); $i++) {
+            $url = str_replace('l.', 't.', $thumbs[$i]['img_url']);
+            echo '<a href="view.php?spp='.$thumbs[$i]['latin_name'].'">';
+            if ($url != '') echo '<img src="'.$url.'" width=80></a>';
+            else echo '<div style="width:80px; height:80px; display:inline-block; background-color:#e9ecef; vertical-align: middle">&nbsp;</div></a>';
+          }
+        ?>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 <?php
