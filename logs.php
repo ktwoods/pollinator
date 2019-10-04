2694efc9ae23958ba5930389b05aedd0393cedba
Katie Woods <kfw8@hood.edu>
2019-10-04 17:40:47 -0400

    Added documentation to logs and new_log

--- logs.php
+++ logs.php
@@ -26,21 +26,31 @@ else if ($type == 'other') {
 	$btn_class = 'o';
 }
 
-include 'header.html';
+include_once 'header.html';
 include_once 'connect.php';
 include_once 'funcs_general.php';
 
-# For hyperlinking within log notes:
-# Binary search to check if a two-word phrase is a valid species name
-function matches_plant($plants, $spp, $start, $end) {
+// Get array of plant species names
+$stmt = $conn->prepare("SELECT latin_name FROM Plant");
+$stmt->execute();
+$plant_names = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+/* Binary search to check if a two-word phrase is a valid plant species name.
+	 Returns name of plant if found, or nothing if not found. Used for hyperlinking
+	 within log notes.
+
+	 $spp = two-word string to search for in plant species list
+	 $start = starting index for search
+	 $end = ending index for search */
+function matches_plant($spp, $start, $end) {
+	global $plant_names;
+
 	if ($end < $start) return;
 	$mid = (int)(($start + $end)/2);
-	$mid_item = $plants[$mid]['latin_name'];
-	if (strcasecmp($spp, $mid_item) > 0) return matches_plant($plants, $spp, $mid+1, $end);
-	else if (strcasecmp($spp, $mid_item) < 0) return matches_plant($plants, $spp, $start, $mid-1);
-	else {
-		return $mid_item;
-	}
+	$mid_item = $plant_names[$mid]['latin_name'];
+	if (strcasecmp($spp, $mid_item) > 0) return matches_plant($spp, $mid+1, $end);
+	else if (strcasecmp($spp, $mid_item) < 0) return matches_plant($spp, $start, $mid-1);
+	else return $mid_item;
 }
 ?>
 
@@ -53,35 +63,31 @@ function matches_plant($plants, $spp, $start, $end) {
 			$stmt->execute();
 			$logs = $stmt->fetchAll();
 			$year = $logs[0]['date'];
-			?>
-			<table style="width: 100%">
-			<?php
-			# Get array of plant species names
-			$stmt = $conn->prepare("SELECT latin_name FROM Plant");
-			$stmt->execute();
-			$plant_names = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			echo '<table style="width: 100%">';
 
-			# Print each log and the enclosing tables
+			// Print each log and the enclosing tables
 			foreach ($logs as $log) {
-				# If we've hit a new year, end the previous table and start a new one
-				if (substr($log['date'], 0, 4) != $year) :
+				// If we've hit a new year, end the previous table and start a new one
+				if (substr($log['date'], 0, 4) != $year) {
 					$year = substr($log['date'], 0, 4);
 					$stmt = $conn->prepare("SELECT COUNT(DISTINCT latin_name) AS num_spp, COUNT(latin_name) as num_logs from $table ".(strpos($table, " WHERE ") === FALSE ? "WHERE" : "AND")." date LIKE '$year%'");
 					$stmt->execute();
 					$stats = $stmt->fetch(PDO::FETCH_ASSOC);
-					?>
-			</table>
-			<h1 class="text-center"><small><?php echo $year.' ('.$stats['num_logs'].' log'.($stats['num_logs'] == 1 ? '' : 's').', '.$stats['num_spp'].' spp)' ?></small></h1>
-			<table style="width: 100%">
-				<th colspan=2>&nbsp;</th><th>Name</th><th>Date</th><th>Notes</th>
-			<?php endif; # END OF IF STATEMENT ?>
+
+					echo '</table>'
+							 . '<h1 class="text-center"><small>'
+							 . $year . ' (' . $stats['num_logs'] . ' log' . ($stats['num_logs'] == 1 ? '' : 's') . ', ' . $stats['num_spp'].' spp)'
+							 . '</small></h1>'
+							 . '<table style="width: 100%">'
+							 . '<th colspan=2>&nbsp;</th><th>Name</th><th>Date</th><th>Notes</th>';
+				} ?>
 				<tr>
 					<!-- Edit button -->
 					<td><a href="edit_log.php?name=<?php echo $log['latin_name'] ?>&date=<?php echo $log['date'] ?>&stage=<?php echo $log['stage'] ?>" class="btn btn-<?php echo $btn_class ?>">
 						<i class="fas fa-edit"></i>
 					</a></td>
 					<!-- Thumbnail -->
-					<td><?php thumbnail($log['img_url'], $log['latin_name'], "3rem"); ?></td>
+					<td><?php thumbnail($log['img_url'], $log['latin_name'], '3rem'); ?></td>
 					<!-- Species name -->
 					<td style="white-space: nowrap"><?php echo $log['common_name'].'<br/><em>(<a href="view.php?spp='.$log['latin_name'].'">'.$log['latin_name'].'</a>)</em>' ?></td>
 					<!-- Date -->
@@ -89,35 +95,35 @@ function matches_plant($plants, $spp, $start, $end) {
 					<!-- Notes -->
 					<td>
 						<?php
-						# Adds hyperlinks to species pages within notes field
+						/* Adds hyperlinks to species pages within notes field */
+						// Split notes into words
 						$notes = explode(" ", $log['notes']);
 						for ($i = 0; $i < count($notes) - 1; $i++) {
-							# Remove leading/trailing punctuation for search
+							// Remove leading/trailing punctuation for search
 							$genus = ltrim($notes[$i], "()[].,:;!?");
 							$spp = rtrim($notes[$i+1], "()[].,:;!?");
-							# Search
-							$match = matches_plant($plant_names, $genus.' '.$spp, 0, count($plant_names) - 1);
+							// Search
+							$match = matches_plant($genus.' '.$spp, 0, count($plant_names) - 1);
 							if ($match) {
-								# Determine where to start/end hyperlink
+								// Determine where to start/end hyperlink (avoids including any punctuation in the hyperlink)
 								$startdif = strlen($notes[$i]) - strlen($genus);
 								$enddif = strlen($notes[$i+1]) - strlen($spp);
-								# Add opening tag
+								// Add opening tag
 								$notes[$i] = substr($notes[$i], 0, $startdif)."<a href='view_plant.php?spp=$match'>".substr($notes[$i], $startdif);
-								# Add closing tag
+								// Add closing tag
 								$notes[$i+1] = substr($notes[$i+1], 0, strlen($notes[$i+1])-$enddif)."</a>".substr($notes[$i+1], strlen($notes[$i+1])-$enddif, $enddif);
-								# Advance index beyond end of species name
+								// Advance index beyond end of species name
 								$i++;
 							}
 						}
-						echo implode(" ", $notes); # Recombine into string
+						echo implode(" ", $notes); // Recombine into string and print
 						?>
 					</td>
 				</tr>
-			<?php } # END OF LOG-PRINTING LOOP ?>
+			<?php } // END OF LOG-PRINTING LOOP ?>
 			</table>
 			<p>&nbsp;</p>
 		</div>
 	</div>
 </div>
-
-<?php include 'footer.html'; ?>
+<?php include_once 'footer.html'; ?>
--- new_log.php
+++ new_log.php
@@ -1,6 +1,6 @@
 <?php
 $cur_page = 'new_log';
-include 'header.html';
+include_once 'header.html';
 include_once 'connect.php';
 include_once 'funcs_general.php';
 ?>
@@ -15,6 +15,7 @@ $(document).ready(function(){
 
 <div class="container-fluid">
 	<?php
+  // Attempt to submit log, and print success/fail alert
 	if (isset($_POST['latin'])) // Submitting data
 	{
 		$latin = $_POST['latin'];
@@ -32,7 +33,7 @@ $(document).ready(function(){
 		{
 			$rows_affected = $stmt->rowCount();
 			if ($rows_affected != 0) echo "<div class='alert alert-success alert-dismissable text-center'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>New log added for <em>$latin</em>, $date!</div>";
-			else echo "<div class='alert alert-warning alert-dismissable text-center'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>Error: unable to add entry for <em>$latin</em> on $date to the database.</div>";
+			else echo "<div class='alert alert-warning alert-dismissable text-center'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>Error: unable to add entry for <em>$latin</em>, $date to the database.</div>";
 		}
 	}
 	?>
@@ -41,6 +42,7 @@ $(document).ready(function(){
 	<form action="new_log.php" method="post">
 		<div class="row justify-content-center">
 			<div class="col-sm-6">
+        <!-- Date -->
 				<div class="form-group" style="margin-top: 10px">
 					<label for="date">Date</label>
 					<?php
@@ -53,7 +55,7 @@ $(document).ready(function(){
 					<small id="dateHelp" class="form-text text-muted">YYYY-MM-DD</small>
 				</div>
 
-				<!-- Latin name -->
+				<!-- Species name selector -->
 				<div class="form-group">
 					<label for="latin">Species</label>
 					<input class="form-control" id="searchSpp" type="text" placeholder="Search species">
@@ -68,22 +70,24 @@ $(document).ready(function(){
 						?>
 					</select>
 				</div>
+        <!-- Stage ('adult' selected by default) -->
 				<div class="form-check checkbox-inline" id="stage">
 					<input class="form-check-input" type="radio" name="stage" value="larva" id="larva"><label class="form-check-label" for="larva" style="margin-left: 10px">Larva</label>
 				</div>
 				<div class="form-check checkbox-inline">
 					<input class="form-check-input" type="radio" name="stage" value="adult" id="adult" checked><label class="form-check-label" for="adult" style="margin-left: 10px">Adult</label>
 				</div>
-
+        <!-- Notes -->
 				<div class="form-group">
 					<label for="notes" style="margin-top: 1em">Notes</label>
 					<textarea class="form-control" id="notes" name="notes"></textarea>
 				</div>
 			</div>
 		</div>
+    <!-- 'Create' button -->
 		<div class="row justify-content-center"><div class="col-1"><button type="Submit" class="btn btn-d" style="color: white">Create</button></div></div>
 	</form>
 	<div>&nbsp;</div>
 </div>
 
-<?php include 'footer.html'; ?>
+<?php include_once 'footer.html'; ?>
